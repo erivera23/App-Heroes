eccfdf1e46a7ca899d69d212eaed634d3857ff9f
Efrain Rivera <riveraefrain5@gmail.com>
2020-02-03 20:10:05 -0600

    Recibe información componente hijo de padre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { BusquedaheroesComponent } from './components/busquedaheroes/busquedaheroes.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { BusquedaheroesComponent } from './components/busquedaheroes/busquedaher
     AboutComponent,
     HeroesComponent,
     HeroeComponent,
-    BusquedaheroesComponent
+    BusquedaheroesComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
